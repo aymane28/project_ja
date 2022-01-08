eb0698547ba66e4b445a4bad0a8464c837d0538d
aymane28 <aymanedhj7@gmail.com>
2022-01-09 00:10:41 +0100

    Add controller first API

---
+++ site/src/main/java/fr/lernejo/travelsite/controller/InscriptionController.java
@@ -0,0 +1,42 @@
+package fr.lernejo.travelsite.controller;
+
+
+import fr.lernejo.travelsite.model.Inscription;
+import fr.lernejo.travelsite.model.Travels;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class InscriptionController {
+    private final Inscription inscription = null;
+
+    @PostMapping("/api/inscription")
+    @ResponseBody
+    public Inscription createProduct(@RequestBody Inscription inscription) {
+        /*JSONObject obj = new JSONObject();
+        FileWriter  file = null;
+        obj.put("userEmail", inscription.getUserEmail());
+        obj.put("userName", inscription.getUserName());
+        obj.put("userCountry", inscription.getUserCountry());
+        obj.put("weatherExpectation", inscription.getWeatherExpectation());
+        obj.put("minimumTemperatureDistance", inscription.getMinimumTemperatureDistance());
+        try {
+             file = new FileWriter("site/src/main/resources/json/inscription.json");
+            file.write(obj.toJSONString());
+        } catch (IOException e) {e.printStackTrace();}
+                try {file.close();
+            } catch (IOException e) {e.printStackTrace();}/*/
+        return inscription;
+    }
+
+    @GetMapping("/api/travels")
+    public ArrayList<Travels> getTravels(@RequestParam(value = "userName") String userName) throws Exception {
+        //return siteService.getTravels();
+        Travels tra= new Travels("France", 24.2);
+        ArrayList<Travels> travels= new ArrayList<>();
+        travels.add(tra);
+        return travels;
+    }
+}
