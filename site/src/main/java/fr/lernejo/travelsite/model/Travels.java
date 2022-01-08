5b432df5054065f170c2afca12f8e8ea6cb71ddd
aymane28 <aymanedhj7@gmail.com>
2022-01-09 00:09:13 +0100

    Add travels class

---
+++ site/src/main/java/fr/lernejo/travelsite/model/Travels.java
@@ -0,0 +1,25 @@
+package fr.lernejo.travelsite.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Travels {
+
+    private final String country;
+    private final Double temperature;
+
+    @JsonCreator
+    public Travels(@JsonProperty("country") String country, @JsonProperty("temperature") Double temperature) {
+        this.country = country;
+        this.temperature = temperature;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+}
