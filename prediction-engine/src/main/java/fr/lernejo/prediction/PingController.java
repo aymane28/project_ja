9e8a307969529e224b41c93774cf093ee545ecea
aymane28 <aymanedhj7@gmail.com>
2022-01-09 00:27:15 +0100

    Add controller second API & Calcul prediction

--- prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/PingController.java
@@ -1,13 +1,30 @@
 package fr.lernejo.prediction;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
 
+    @Autowired
+    private final PredictionService predictionService;
+
+    public PingController(TemperatureService temperatureService, PredictionService predictionService) {
+        this.predictionService = predictionService;
+    }
+
     @GetMapping(path = "/api/ping")
     String ping() {
         return "OK";
     }
+
+    @GetMapping("/api/temperature")
+    public Temperature getTravels(@RequestParam(value = "country") String country){
+        Temperature temperature= new Temperature(country, predictionService.temperature(country));
+
+        return temperature;
+    }
+
 }
