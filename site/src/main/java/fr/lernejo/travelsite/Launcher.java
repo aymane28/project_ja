108a66ef0e3c215ba08050a7d1225ec0b89d6896
aymane28 <aymanedhj7@gmail.com>
2022-01-09 00:08:15 +0100

    Add Inscription class

--- site/src/main/java/fr/lernejo/travelsite/Launcher.java
+++ site/src/main/java/fr/lernejo/travelsite/Launcher.java
@@ -2,8 +2,9 @@ package fr.lernejo.travelsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Launcher {
 
     public static void main(String[] args) {
